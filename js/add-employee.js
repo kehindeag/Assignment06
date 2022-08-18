b7c6d889978da382f305d40aa74f51233bda7162
kehindeag <kenagboola@gmail.com>
2022-08-17 21:57:36 -0700

    final work

--- js/add-employee.js
+++ js/add-employee.js
@@ -1,24 +1,39 @@
-// HANDLE THE LOAD EVENT OF THE WINDOW
+// GLOBAL VARIABLES
+let id; // HTML ELEMENT (TEXT)
+let fname; // HTML ELEMENT (TEXT)
+let ext; // HTML ELEMENT (TEXT)
+let email; // HTML ELEMENT (TEXT)
+let department; // HTML ELEMENT (TEXT)
+let submit; // HTML ELEMENT (BUTTON)
+let output; // HTML ELEMENT (OUTPUT)
 
-// SET WIDTH AND HEIGHT VARIABLES TO 800 x 700
+// HELPER FUNCTION TO GET DOM ELEMENTS
+const $ = (i) => document.getElementById(i);
 
-// RESIZE THE POPUP TO THE SET WIDTH AND HEIGHT
+// GET DOM ELEMENTS
+id = $("id");
+fname = $("fname");
+ext = $("ext");
+email = $("email");
+department = $("department");
+submit = $("submit");
+output = $("output");
 
-// MOVE THE POPUP TO THE MIDDLE OF THE SCREEN
+// CONSOLE LOG OR PUT IN PAGE USING innerHTML AND RETURN THE RESULT
+function employeeDetail(id, fname, ext, email, department) {
+  console.log(`ID: ${id.value}`);
+  console.log(`Full Name: ${fname.value} `);
+  console.log(`Extension: ${ext.value}`);
+  console.log(`Email: ${email.value}`);
+  console.log(`Department: ${department.value}`);
+  //output.innerHTML = `ID: ${id.value} <br> Full Name: ${fname.value} <br> Extension: ${ext.value} <br> Email: ${email.value} <br> Department: ${department.value}`;
+  return;
+}
 
-// CREATE A HELPER FUNCTION TO RETRIEVE THE HTML ELEMENTS FROM THE DOM
-
-// HANDLE THE CANCEL BUTTON. WHEN THE USER CLICKS THIS BUTTON, CLOSE THE WINDOW
-
-// HANDLE THE SUBMISSION OF THE FORM AND THEN IMMEDIATELY PREVENT THE SUBMISSION
-
-// CREATE 5 VARIABLES FOR ID, NAME, EXT, EMAIL, AND DEPT
-// SET THOSE VARIABLES TO WHATEVER THE USER ENTERS INTO THE FORM ELEMENTS
-
-// GET THE LOGINDETAILS OUTPUT ELEMENT FROM THE PARENT PAGE
-
-// SET THE TEXT OF THE LOGINDETAILS ELEMENT TO THE ABOVE SET VARIABLES
-
-// CLOSE THE POPUP
-
-// THE DATA SHOULD SHOW ON THE INDEX.HTML PAGE
+// GET FORM VALUES ,LOG AND  DISPLAY RESULTS
+submit.addEventListener("click", () => {
+  employeeDetail(id, fname, ext, email, department);
+  let parentoutput = window.opener.document.getElementById("loginDetails");
+  parentoutput.innerHTML = `ID: ${id.value} <br> Full Name: ${fname.value} <br> Extension: ${ext.value} <br> Email: ${email.value} <br> Department: ${department.value}`;
+  window.close();
+});
--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
+// GLOBAL VARIABLES
+let btnAddEmployee; // HTML ELEMENT (TEXT)
+let loginDetails; // HTML ELEMENT (TEXT)
+
+// HELPER FUNCTION TO GET DOM ELEMENTS
+const $ = (i) => document.getElementById(i);
+
+// GET DOM ELEMENTS
+btnAddEmployee = $("btnAddEmployee");
+loginDetails = $("loginDetails");
+
 // HANDLE THE LOAD EVENT OF THE WINDOW
 
 // HANDLE THE CLICK EVENT FOR THE BTNADDEMPLOYEE BUTTON
-
+btnAddEmployee.addEventListener("click", () => {
+  window.open("add-employee.html", "popup", "width = 600, height = 800");
+});
 // OPEN THE ADD-EMPLOYEE.HTML PAGE WITHIN A POPUP
